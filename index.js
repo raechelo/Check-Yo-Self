8838f9d23769b69c88102491db0d3209d55a9368
raechel <odom.raechel@gmail.com>
2019-05-16 20:25:53 -0600

    diff color scheme added

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // variables
 const taskTitle = document.getElementById('task-title');
-const taskTodo = document.getElementById('')
+const taskTodo = document.getElementById('task-item-js');
+const addTodoBtn = document.getElementById('task-btn-js');
+const addTaskBtn = document.getElementById('add-task-js');
+const clearBtn = document.getElementById('clear-btn');
+const listSection = document.getElementById('tasks-to-add-js');
+const taskSection = document.getElementById('todo-js');
+const todos = [];
 
 // event listeners
+addTodoBtn.addEventListener('click', addTodo);
+addTaskBtn.addEventListener('click', addTask);
+clearBtn.addEventListener('click', clearTasks);
+
+// functions
+
+function appendTodos(array) {
+  const displayTodos = array.map(t => {
+    new Todo(t.title, t.tasks, t.id, t.urgent)
+  })
+}
+
+function addTodo() {
+  listSection.append(taskTodo.value);
+}
+
+function addTask(task) {
+  taskSection.innerHTML += `<article class="card">
+  <h3>${task.title}</h3>
+  <hr>
+  <li>${task.todo}</li>
+  <hr>
+  <button><i class="fas fa-exclamation"></i></button>
+  <button><i class="far fa-times-circle"></i></button>
+</article>`
+}

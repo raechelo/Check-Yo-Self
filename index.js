7540116b0bf78437831d303d12841e748f71ee2b
raechel <odom.raechel@gmail.com>
2019-05-16 22:43:00 -0600

    cards now populate on dom and disappear properly. kind of buggy atm

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
 // variables
-const taskTitle = document.getElementById('task-title');
+const taskTitle = document.getElementById('task-title-js');
 const taskTodo = document.getElementById('task-item-js');
 const addTodoBtn = document.getElementById('task-btn-js');
 const addTaskBtn = document.getElementById('add-task-js');
 const clearBtn = document.getElementById('clear-btn');
 const listSection = document.getElementById('tasks-to-add-js');
 const taskSection = document.getElementById('todo-js');
-const todos = [];
+const delBtn = document.getElementById('delete-btn-js');
+const urgentBtn = document.getElementById('urgent-btn-js');
+let todos = [];
+let tasks = [];
 
 // event listeners
 addTodoBtn.addEventListener('click', addTodo);
-addTaskBtn.addEventListener('click', addTask);
-clearBtn.addEventListener('click', clearTasks);
+addTaskBtn.addEventListener('click', createTask);
+taskSection.addEventListener('click', deleteTask)
+// clearBtn.addEventListener('click', clearTasks);
 
 // functions
 
-function appendTodos(array) {
-  const displayTodos = array.map(t => {
-    new Todo(t.title, t.tasks, t.id, t.urgent)
-  })
-}
-
 function addTodo() {
-  listSection.append(taskTodo.value);
+  listSection.innerHTML += `<p>${taskTodo.value}</p>`;
+  tasks.push(taskTodo.value)
+  taskTodo.value = '';
 }
 
-function addTask(task) {
-  taskSection.innerHTML += `<article class="card">
-  <h3>${task.title}</h3>
-  <hr>
-  <li>${task.todo}</li>
-  <hr>
-  <button><i class="fas fa-exclamation"></i></button>
-  <button><i class="far fa-times-circle"></i></button>
-</article>`
+function createTask() {
+  const newTodo = {title: taskTitle.value, tasks: tasks, id:Date.now()}
+  todos.push(newTodo)
+  addTask(newTodo)
+  clearFields();
+  tasks = [];
+}
+
+// function mapTasks(todos) {
+//   return todos.map(t => addTask(t))
+// }
+
+function addTask(t) {
+  let todo = `<article id=${t.id} class="card">
+    <h3>${t.title}</h3>
+    <hr>
+    ${t.tasks.map(a => a)}
+    <hr>
+    <button class="urgent-btn" id="urgent-btn-js"><i class="fas fa-exclamation"></i></button>
+    <button class="delete-btn" id="delete-btn-js"><i class="far fa-times-circle"></i></button>
+  </article>`
+  taskSection.insertAdjacentHTML('beforeend', todo)
+}
+
+function clearFields() {
+  taskTitle.value = ''
+  taskTodo.value = ''
+  listSection.innerHTML = ''
+}
+
+function deleteTask(e) {
+  todos = todos.filter(t => t.id !== parseInt(e.target.parentElement.id));
+  taskSection.innerHTML = '';
+  todos.map(t => addTask(t));
+  console.log(todos)
 }
